b7f45ea4d518bbe5e4d702670a20e71306625b9f
ysergma <sil3nthilldz@gmail.com>
2023-09-30 21:28:00 +0200

    feat(footer component): add the footer component of the website
    "fix #18"

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,7 @@ import StarRating from "@/components/Rating/StarRating"
 import Hero from "@/components/Hero/Hero"
 import ShoppingCard from "@/components/ShoppingCard/ShoppingCard"
 import Link from "next/link"
+import Footer from "@/components/Footer/Footer"
 export default function Home() {
   const [products, setProducts] = useState([])
 
@@ -15,28 +16,7 @@ export default function Home() {
 
   return (
     <>
-      <Hero />
-
-      <h1
-        className="text-4xl font-bold mb-4 text-text-brown flex justify-center items-center"
-        style={{ fontFamily: "Mooli" }}
-      >
-        Products List
-      </h1>
-      <div className="border-b border-gold-g mb-6"></div>
-      <div className="grid grid-cols-3 gap-6 ml-16 mr-16 my-10">
-        {products.map((product) => (
-          <ShoppingCard key={product.id} {...product} />
-        ))}
-      </div>
-
-      <div className="flex justify-center my-6  ">
-        <Link href="/products" target="_BLANK">
-          <button className="btn bg-buttongold text-brown-text-brown px-4 py-2 rounded-full hover:bg-buttongoldhov mb-4">
-            see more products
-          </button>
-        </Link>
-      </div>
+      <Footer />
     </>
   )
 }
