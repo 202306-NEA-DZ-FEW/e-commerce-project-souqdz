29bd732b9ff5c633988d33caca37d412be72fdc0
hasna <manelhasnahaddoud@gmail.com>
2023-09-30 01:10:36 +0100

    feat(navbar): add search functionality

--- src/components/Navbar/SearchBar.jsx
+++ src/components/Navbar/SearchBar.jsx
@@ -1,11 +1,38 @@
 import React from "react"
 import { BiSearch } from "react-icons/bi"
+import { useState, useEffect } from "react"
+import ShoppingCard from "../ShoppingCard/ShoppingCard"
 
 const SearchBar = () => {
+  const [searchQuery, setSearchQuery] = useState("")
+  const [result, setresult] = useState("")
+
+  function fetchingProducts() {
+    fetch("https://fakestoreapi.com/products")
+      .then((res) => res.json())
+      .then((data) => setresult(data))
+      .catch((error) => console.error("Error fetching data:", error))
+  }
+  useEffect(() => {
+    fetchingProducts()
+  }, [searchQuery])
+  const handleSearch = async (e) => {
+    e.preventDefault()
+    fetchingProducts()
+    // setSearchQuery("")
+  }
+  const itemsToDisplay = [...result].filter((item) => {
+    if (item.title) {
+      return item.title.toLowerCase().includes(searchQuery.toLowerCase())
+    }
+    return false
+  })
+  console.log(itemsToDisplay)
+
   return (
     <div>
       <div className="flex items-center bg-gray-100 p-2 rounded-full max-md:hidden">
-        <button>
+        <button onClick={handleSearch}>
           <BiSearch size={20} className="opacity-50" />
         </button>
         <input
@@ -13,8 +40,15 @@ const SearchBar = () => {
           className="outline-none bg-transparent ml-2 caret-blue-500 placeholder:font-light  placeholder:text-gray-600 text-[15px]"
           placeholder="Search"
           autoComplete="false"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
         />
       </div>
+      {/* <div className="flex flex-wrap">
+        {itemsToDisplay.map((item) => (
+          <ShoppingCard key={item.id} product={item} />
+        ))}
+      </div> */}
     </div>
   )
 }
